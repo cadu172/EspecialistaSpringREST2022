e767035bfda84f3c4d8895fb23059baf20aa5b7b
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-10-30 20:05:30 -0300

    ESR - 02.18 - Desambiguação de beans com @Qualifier

--- algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
@@ -1,11 +1,31 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
-@Primary
+
+/*
+ * Quando temos dois Beans que implementam a mesma interface e uma classe também Bean faz a injeção de dependencia o Spring acusa um erro informando que houve uma
+ * ambiguidade, ou seja, tem dois Beans iguais carregados para serem injetados.
+ * Um forma de resolver isso é alterar o construtor ou atribuito com a marcação @Autowired para um tipo List<> do tipo da Interface, desta forma o Spring passa todos os 
+ * Beans para esta lista e você pode usar da forma que foi mais conveniente.
+ * Caso você não queira priorizar você precisa informar ao Spring qual o Bean tem prioridade de injeção na classe, então você uma anotação chamada @Primary
+ * */
+
+
+/*
+ * No lugar da anotação @Primary podemos usar a anotação @Qualifier("aliasname"), anotação permite criar um apelido para o Bean, é mais prático que usar o
+ * Primary porque quando for necessário fazer injeção basta incluir acima do atributo @Autowired a anotação com o apelido criado.
+ * No exemplo abaixo criamos 2 anotações  @Qualifier("notificacao.normal") e @Qualifier("notificacao.urgente"), 
+ * na classe AtivacaoClienteService chamamos por exemplo a anotação @Qualifier("notificacao.urgente")
+ * com isso o Spring Injeta nesta classe o Bean NotificadorSMS que foi o Bean com aplidado de urgente.
+ * */
+
+//@Primary
+@Qualifier("notificacao.normal")
 @Component
 public class NotificadorSMS implements InterfaceNotificador {
 	
--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,8 +1,7 @@
 package com.algaworks.algafood.di.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -15,6 +14,7 @@ public class AtivacaoClienteService {
 	 * Podemos incluir uma parâmetro "required em @Autowired", desta forma dizemos ao Spring se ele é obrigatório ou não, o valor padrão é true
 	 * Caso seja definido como true é obrigatório que um Bean deste tipo seja carregado no IoC Container
 	 * */
+	@Qualifier("notificacao.urgente")
 	@Autowired(required = true)
 	private InterfaceNotificador notificador;
 	

5e4465eeacfbf0389707c6a828d1018b22f521dd
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-11-01 23:27:44 -0300

    ESR - 02.25 - Criando e acessando propriedades customizadas com @Value

--- algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +13,29 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements InterfaceNotificador {
 	
+	/*
+	 * A anotação @Value permite capturar valores do arquivo application.properties, de forma geral utilizamos este arquivo para as configurações
+	 * do aplicativo, isso é necessário para evitar que seja utilizado um "Hardcode", ou seja, que seja registrado no código fonte do aplicativo configurações
+	 * que podem mudar de ambiente para ambiente, por exemplo: Uma mudança na rede interna da empresa pode exigir uma recompilação da aplicação e alteração de 
+	 * componentes quem não deveriam ser mexidos naquele momento.
+	 * 
+	 * */
+	
+	@Value("${notificador.email.smtp-server}")
+	private String smtpServer;
+	
+	@Value("${notificador.email.smtp-port}")
+	private Integer smtpServerPort;
+	
 	public NotificadorEmail() {
 		System.out.println("NotificadorEmail");
 	}
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		
+		System.out.println("SMTP-SERVER: " + this.smtpServer);
+		System.out.println("SMTP-PORT: " + this.smtpServerPort);
 		
 		System.out.println("PRODUCAO ----- Cliente " + cliente.getNome() +
 					" notificado por EMAIL atraves do endereço " + cliente.getEmail() +

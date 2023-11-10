7160b0bdfc0bfda92c60c511925f2c3d535eafc5
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-11-10 06:08:25 -0300

    ESR - 03.11 - Atualizando um objeto no banco de dado

--- algafood-api/src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 	
 	public static void main(String[] args) {
 		
@@ -17,18 +17,12 @@ public class InclusaoCozinhaMain {
 		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
+		Cozinha cozinha = new Cozinha();
+		cozinha.setNome("Brasileira");
+		cozinha.setId(1L);		
 		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Japonesa");
+		cadastroCozinha.salvar(cozinha);
 		
-		cadastroCozinha.salvar(cozinha1);
-		cadastroCozinha.salvar(cozinha2);
-		
-		
-		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
 		
 	}
 

a4ed333cb438a76710e1c76d885d6120516aa46e
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-11-10 11:45:55 -0300

    ESR - 03.12 - Excluindo um objeto do banco de dados

--- algafood-api/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -30,5 +30,18 @@ public class CadastroCozinha {
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
+	
+	@Transactional
+	public void remover(Cozinha cozinha) {
+		
+		/*
+		 * só é possível excluir objetos gerenciados, no caso o objeto cozinha que foi passado não é gerenciado e está "detached"
+		 * Basta fazer a busca que ele se torna gerenciado e habilita a possibilidade de exclusao
+		 * */
+		
+		cozinha = this.buscar(cozinha.getId()); // aqui o objeto passa a ser gerenciado porque foi retornado pelo método find do "EntityManager"
+		
+		manager.remove(cozinha);
+	}	
 
 }

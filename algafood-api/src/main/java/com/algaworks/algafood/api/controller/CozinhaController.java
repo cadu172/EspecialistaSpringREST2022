2d1caabe99dc5fb01d71d600692f625b0bf97a3e
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-11-19 16:28:38 -0300

    ESR - 04.23 - Modelando e implementando a inclusão de recursos com POST

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,16 +1,15 @@
 package com.algaworks.algafood.api.controller;
 
-import java.net.http.HttpResponse;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +54,12 @@ public class CozinhaController {
 		
 		return ResponseEntity.ok(cozinha);		
 		
-	}	
+	}
+	
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Cozinha salvar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
 
 }

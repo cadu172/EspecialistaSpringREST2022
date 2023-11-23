92fa2c7ba3e40a17457164ae919b86303a041a56
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-11-23 20:44:53 -0300

    ESR - 04.34 - Finalizando a atualização parcial com a API de Reflections do Spring

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,11 +1,13 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping(value = "/restaurantes")
@@ -106,16 +109,50 @@ public class RestauranteController {
 		
 		Restaurante restauranteDestino = cadastroRestauranteService.buscar(restauranteId);
 		
+		// faz o merge somente dos campos enviados na consulta, o restante ele não altera
 		merge(requestBody, restauranteDestino);
 		
-		return ResponseEntity
-				.status(HttpStatus.OK).build();
+		try {
+			
+			restauranteDestino = cadastroRestauranteService.alterar(restauranteDestino);
+			
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(restauranteDestino);					
+			
+		}
+		catch (EntidadeNaoEncontradaException e) {
+
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body(e.getMessage());
+			
+		}
 		
 	}
 
-	private void merge(Map<String, Object> restauranteOrigem, Restaurante restauranteDestino) {
-		restauranteOrigem.forEach((nomeDoCampo, valorDoCampo) -> {
-			System.out.println(nomeDoCampo + " = " + valorDoCampo);
+	private void merge(Map<String, Object> requestBody, Restaurante restauranteDestino) {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		// converte o requestBody em um tipo Restaurantes
+		Restaurante restauranteOrigem = objectMapper.convertValue(requestBody, Restaurante.class);		
+		
+		// percorre o requestBody para "fazer um set" das requisições no objeto destino
+		requestBody.forEach((chave, valor) -> {
+			
+			// Este Reflections procura no objeto o "atributo atual" armazenado na variável chave
+			Field nomeCampo = ReflectionUtils.findField(Restaurante.class, chave);
+			
+			// coverte o atributo da classe de private para public em tempo de execução
+			nomeCampo.setAccessible(true);
+			
+			// Este reflection procura o campo encontrado no requestBody e obtem o valor do campo
+			Object novoValor = ReflectionUtils.getField(nomeCampo, restauranteOrigem);
+			
+			// Este utilitário altera no objeto final (encontrado no banco) o valor passado no requestBody no campo atual
+			ReflectionUtils.setField(nomeCampo, restauranteDestino, novoValor);
+			
 		});
 	}	
 

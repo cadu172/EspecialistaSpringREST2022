c9f6196f794a7254860b484959b0815d374b6e56
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-11-23 01:31:27 -0300

    ESR - 04.32 - Desafio - implementando serviços REST de cidades e estados (CONCLUIDO)

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.service.CadastroCidadeService;
@@ -134,6 +135,11 @@ public class CidadeController {
 					.status(HttpStatus.NOT_FOUND)
 					.body(e.getMessage());
 		}
+		catch (EntidadeEmUsoException e) {
+			return ResponseEntity
+					.status(HttpStatus.CONFLICT)
+					.body(e.getMessage());
+		}		
 		catch (Exception e) {
 			return ResponseEntity
 					.status(HttpStatus.BAD_REQUEST)

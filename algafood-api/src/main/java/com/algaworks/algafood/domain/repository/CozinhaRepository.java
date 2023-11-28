6073fbee7b5ed7aac773537be47a12e42303c602
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-11-28 18:22:25 -0300

    ESR - 05.06 - Criando consultas com query methods

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +11,25 @@ import com.algaworks.algafood.domain.model.Cozinha;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 	
-	//List<Cozinha> consultarPorNome(String nomeDaCozinha);	
+	// só de declarar asssim o Spring cria automáticamente uma rotina que faz a busca por nome (exato)
+	//List<Cozinha> nome(String nomeDaCozinha);	
 	
+	// Desta forma também faz a busca exata, basta informar o prefixo "findBy" + "nome do campo", conforme exemplo abaixo
+	// exemplo: findByNome
+	//List<Cozinha> findByNome(String nomeDaCozinha);
+	
+	/*
+	 * quais são os prefixos?
+	 * 
+	 * "find" e "By"
+	 * 
+	 * Voce pode compor um método com nome customizado, exemplo:  findTodasAsCozinhaByNome
+	 * 
+	 * */
+	//obtem uma lista de cozinhas
+	List<Cozinha> findTodasAsCozinhaByNome(String nomeDaCozinha);
+	
+	
+	// obtem uma unica cozinha
+	Optional<Cozinha> findCozinhaByNome(String nomeDaCozinha);
 }

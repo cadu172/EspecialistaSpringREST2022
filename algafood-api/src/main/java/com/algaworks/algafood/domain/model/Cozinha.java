f9ebb6bf4593246ab0036caafc0932dd93ecf69c
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2024-01-30 09:02:24 -0300

    ESR - 07.04 - Schema generation em produção não é uma boa prática

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,5 +1,9 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import jakarta.persistence.Column;
@@ -7,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -25,6 +30,13 @@ public class Cozinha {
 	//@JsonProperty(value = "nomeDoCampoCustomizado")
 	@Column(nullable = false)
 	private String nome;
+	
+	/*@Column(name = "observacao", nullable = false)
+	private String descricao;*/
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 	
 }
 
